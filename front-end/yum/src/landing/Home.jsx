55262a5f534c69d8f3ef85991b41fa214b828243
harvey hunat <harvey.hunat838@adamson.edu.ph>
2024-10-25 13:11:14 +0800

    spacing

--- front-end/yum/src/landing/Home.jsx
+++ front-end/yum/src/landing/Home.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div>
-      <Link to={"/login"}>Login</Link>
-      <Link to={"/register"}>Register</Link>
+      <Link to={"/login"} style={{ marginRight: "50px" }}>Login</Link>
+      <Link to={"/register"} style={{ marginRight: "50px" }}>Register</Link>
       <Link to={"/ordering-home"}>App</Link>
     </div>
   );
